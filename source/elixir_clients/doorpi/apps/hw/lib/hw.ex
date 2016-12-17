a65845c3731ade073ccd2382f9355094956c995f
Roy Veshovda <roy@veshovda.no>
2016-12-17 20:10:58 +0100

    New structure

--- source/elixir_clients/doorpi/apps/hw/lib/hw.ex
+++ source/elixir_clients/doorpi/apps/hw/lib/hw.ex
@@ -1,19 +1,13 @@
 defmodule Hw do
   use Application
 
-  # See http://elixir-lang.org/docs/stable/elixir/Application.html
-  # for more information on OTP Applications
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
 
-    # Define workers and child supervisors to be supervised
     children = [
-      # Starts a worker by calling: Hw.Worker.start_link(arg1, arg2, arg3)
-      # worker(Hw.Worker, [arg1, arg2, arg3]),
+      worker(Hw.GpioWorker, []),
     ]
 
-    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Hw.Supervisor]
     Supervisor.start_link(children, opts)
   end
--- source/elixir_clients/doorpi/apps/hw/mix.exs
+++ source/elixir_clients/doorpi/apps/hw/mix.exs
@@ -14,30 +14,18 @@ defmodule Hw.Mixfile do
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger],
+    [applications: [
+                    :logger,
+                    :elixir_ale,
+                    :logic],
      mod: {Hw, []}]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # To depend on another app inside the umbrella:
-  #
-  #   {:myapp, in_umbrella: true}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:elixir_ale, "~> 0.5.6"}
+      {:elixir_ale, "~> 0.5.6"},
+      {:logic, in_umbrella: true}
     ]
   end
 end
